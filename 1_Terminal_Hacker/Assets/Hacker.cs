c031c329024ec17a88a6473605d40b0d5f61fd3d
CarlosBueloni <carlosbueloni@gmail.com>
2020-02-24 20:29:39 -0300

    Added start screen text

---
+++ 1_Terminal_Hacker/Assets/Hacker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    
+    [SerializeField]
+    [Multiline(10)]
+    private string initialText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Terminal.WriteLine(initialText);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

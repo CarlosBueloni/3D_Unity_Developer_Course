2d0c84dbf5313744473a88a39d1a13c07f8d00f7
CarlosBueloni <carlosbueloni@gmail.com>
2020-03-02 10:23:45 -0300

    Update Hacker.cs
    Starting lecture 25

--- 1_Terminal_Hacker/Assets/Hacker.cs
+++ 1_Terminal_Hacker/Assets/Hacker.cs
@@ -1,77 +1,75 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hacker : MonoBehaviour
-{
+public class Hacker : MonoBehaviour {
     private const string MI_EASTEREGG = "Your mission, should you choose to accept it, is to select a level.";
-    
-    enum Screen
-    {
+
+    enum Screen {
         MainMenu,
         Password,
         Win
-    };
+    }
 
     //Cached Variables
     [SerializeField]
-    [Multiline(10)]
+    [Multiline (10)]
     private string initialText;
-
 
     //Game State
     int level;
-    Screen currentScreen = Screen.MainMenu;
+    Screen currentScreen;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        ShowMainMenu();
+    void Start () {
+        ShowMainMenu ();
     }
 
-    public void ShowMainMenu()
-    {
-        Terminal.ClearScreen();
-        Terminal.WriteLine(initialText);
+    public void ShowMainMenu () {
         currentScreen = Screen.MainMenu;
+        Terminal.ClearScreen ();
+        Terminal.WriteLine (initialText);
 
     }
 
-    void OnUserInput(string input)
-    {
-       if(input == "menu")
+    
+    void OnUserInput (string input) {
+
+        if (input == "menu") // we can always go back to the main menu
         {
-            ShowMainMenu();
+            ShowMainMenu ();
+        } else if (currentScreen == Screen.MainMenu) {
+            RunMainMenu(input);
         }
-        else if(input == "1")
+
+    }
+
+    private void RunMainMenu(string input)
+    {
+        if (input == "1")
         {
             level = 1;
             StartGame();
         }
-        else if(input == "2")
+        else if (input == "2")
         {
             level = 2;
             StartGame();
         }
-        else if(input == "3")
-        {
-            level = 3;
-            StartGame();
-        }
-        else if(input == "MI")
+        else if (input == "MI")
         {
             Terminal.WriteLine(MI_EASTEREGG);
         }
         else
         {
-            Terminal.WriteLine("Please choose a valid level");
+            Terminal.WriteLine("Please enter a valid level");
         }
     }
 
-    void StartGame()
-    {
-        Terminal.WriteLine("You chose level " + level);
-        Terminal.WriteLine("Please type your password: ");
+    void StartGame () {
         currentScreen = Screen.Password;
+        Terminal.WriteLine ("You chose level " + level);
+        Terminal.WriteLine ("Please type your password: ");
     }
 }

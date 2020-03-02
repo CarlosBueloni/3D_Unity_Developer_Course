00e2dfc15bdd16db3c94e3bc87a65adaf559cf99
CarlosBueloni <carlosbueloni@gmail.com>
2020-03-02 13:20:40 -0300

    Update Hacker.cs and Game.unity Added arrays for passwords

--- 1_Terminal_Hacker/Assets/Hacker.cs
+++ 1_Terminal_Hacker/Assets/Hacker.cs
@@ -4,22 +4,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour {
-    private const string MI_EASTEREGG = "Your mission, should you choose to accept it, is to select a level.";
 
-    enum Screen {
-        MainMenu,
-        Password,
-        Win
-    }
-
-    //Cached Variables
+    //Game configuration data
     [SerializeField]
     [Multiline (10)]
     private string initialText;
+    private string[] level1Passwords = { "math", "english", "ruler", "books", "pencil" };
+    private string[] level2Passwords = { "aircratf", "hangar", "propeller", "luggage","runaway"};
 
     //Game State
-    int level;
-    Screen currentScreen;
+    private enum Screen
+    {   MainMenu,
+        Password,
+        Win
+    }
+    private int level;
+    private Screen currentScreen;
+    private string password;
+
 
     // Start is called before the first frame update
     void Start () {
@@ -39,27 +41,32 @@ public class Hacker : MonoBehaviour {
         if (input == "menu") // we can always go back to the main menu
         {
             ShowMainMenu ();
-        } else if (currentScreen == Screen.MainMenu) {
+        } else if (currentScreen == Screen.MainMenu) 
+        {
             RunMainMenu(input);
+
+        } else if(currentScreen == Screen.Password)
+        {
+            CheckPassword(input);
         }
 
     }
+
+
 
     private void RunMainMenu(string input)
     {
         if (input == "1")
         {
             level = 1;
+            password = level1Passwords[2]; // todo make random later
             StartGame();
         }
         else if (input == "2")
         {
             level = 2;
+            password = level2Passwords[4]; // todo make random later
             StartGame();
-        }
-        else if (input == "MI")
-        {
-            Terminal.WriteLine(MI_EASTEREGG);
         }
         else
         {
@@ -69,7 +76,26 @@ public class Hacker : MonoBehaviour {
 
     void StartGame () {
         currentScreen = Screen.Password;
-        Terminal.WriteLine ("You chose level " + level);
+        Terminal.WriteLine ("You chose level " + level+ ".");
+        Terminal.WriteLine("");
         Terminal.WriteLine ("Please type your password: ");
+    }
+
+    private void CheckPassword(string input)
+    {
+        if (input == password)
+        {
+            RunWinScreen();
+        }
+        else
+        {
+            Terminal.WriteLine("Please try again.");
+        }
+    }
+
+    private void RunWinScreen()
+    {
+        currentScreen = Screen.Win;
+        Terminal.WriteLine("Congratulations! You hacked level " + level);
     }
 }
